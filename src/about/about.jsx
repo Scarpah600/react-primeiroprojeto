727350eb9833083ffc5902deb11d86801ac25051
Gustavo Scarpin <40478729+Scarpah600@users.noreply.github.com>
2020-08-24 17:24:16 -0300

    atualizado routes

---
+++ src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <div>
+               <PageHeader name='Sobre' small='Nossa Historia'></PageHeader>
+            </div>
+        
+    </div>
+)
--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,10 +1,15 @@
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
+import './app.css';
 
 import React from 'react'
-
+import Routes from './router'
+import Menu from '../template/menu'
 export default props =>(
     <div className="container">
-        <h1>Teste</h1>
+        <div className="app">
+            <Menu/>
+        <   Routes/>
+        </div>
     </div>
 )
